2387555e349d9aa9304e891d03f437985ecbaba2
aishattursynbaeva <aishat.tursynbaeva@gmail.com>
2019-02-10 21:33:59 +0600

    week2

---
+++ Week2/Task1/Task1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        //создаем функцию, которая проверяет строку на палиндром
+        public static bool Ex(string s)
+        { 
+            for(int i=0; i<s.Length/2; i++)
+            {
+                if (s[i] != s[s.Length-i-1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        //создаем функцию, которая читает строку из файла
+        public static string Ex1()
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            string s = sr.ReadToEnd();
+            sr.Close();
+            return s;
+        }
+
+        static void Main(string[] args)
+        {
+            string s = Ex1();
+            //выводим ответ в другом файле
+            StreamWriter sw = new StreamWriter("output.txt");
+            if (Ex(s) == true)
+            {
+                sw.WriteLine("YES");
+            }
+            else
+            {
+                sw.WriteLine("NO");
+            }
+            sw.Close();
+        }
+    }
+}
---
+++ Week2/Task4/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "aaa.txt"; 
+            string path = @"C:\Users\Айшат\Desktop\PP2";
+            string path1 = @"C:/Users/Айшат/Desktop/PP2/Week2";
+            path=Path.Combine(path, name);//скомбинируем пути
+            FileStream fs = File.Create(path);// создаем файл
+            fs.Close();//остановим работу файла, чтобы скопировать и удалить его
+            path1 = Path.Combine(path1, name);//скомбинируем путь, куда мы должны скопировать
+            File.Copy(path, path1);//скопируем файл
+            File.Delete(path);//удаляем оригинал
+        }
+    }
+}
